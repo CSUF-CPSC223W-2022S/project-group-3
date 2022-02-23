c4ceb31c1ffdb6521349af18045ff3a591af3248
Mariam Mousa <NA>
2022-02-22 18:39:55 -0800

    Initial Commit

---
+++ CSUF_Parking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CSUF_Parking
+//
+//  Created by csuftitan on 2/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
