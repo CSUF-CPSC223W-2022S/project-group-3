5da39fa3490712b7db4ddfd39a9b4832a29f054d
Jonathan Panecatl <jonathanp809@csu.fullerton.edu>
2022-02-24 00:19:29 -0800

    created draft recommendation structure

---
+++ CSUF_Parking/bRecommendation.swift
@@ -0,0 +1,13 @@
+//
+//  bRecommendation.swift
+//  CSUF_Parking
+//
+//  Created by Jonathan Panecatl on 2/24/22.
+//
+
+import Foundation
+
+struct Recommendation {
+    //add more code here
+    //will recommend the parking structure that is closest to the location of your classes
+}
