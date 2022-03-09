fcf99b43725a6b7ef196f65d1879f6397203de8b
KD <NA>
2022-03-08 23:12:23 -0800

    v2

--- CSUF_Parking/weather.swift
+++ CSUF_Parking/weather.swift
@@ -7,8 +7,30 @@
 
 import Foundation
 
-struct weather {
+//let JSON: Data?
+
+//Codable allows decoding JSON data, using JSON example data for now to test
+struct weather: Codable {
     
-    //Add more code here
+    var temp: Double?
+    var humidity: Double?
+    
+}
+
+struct weatherMain: Codable {
+    let main: weather
+    
+}
+
+func decodeJSONData(JSONData: Data){
+    
+    do{
+        let weatherData = try? JSONDecoder().decode(weatherMain.self, from: JSONData)
+        if let weatherData = weatherData{
+            let weather = weatherData.main
+            print(weather.temp!)
+            print(weather.humidity!)
+        }
+    }
     
 }
