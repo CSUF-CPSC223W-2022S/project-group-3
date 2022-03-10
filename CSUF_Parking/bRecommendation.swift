f317ecaa58140e66ed9c6c80ae16f30b55fcddc9
JonathanPanecatl <70716750+JonathanPanecatl@users.noreply.github.com>
2022-03-09 22:47:39 -0800

    Update bRecommendation.swift
    added a class

--- CSUF_Parking/bRecommendation.swift
+++ CSUF_Parking/bRecommendation.swift
@@ -11,3 +11,11 @@ struct Recommendation {
     //add more code here
     //will recommend the parking structure that is closest to the location of your classes
 }
+
+class Best{
+    let name: String
+    
+    func bestStructure{
+        print("The most convient parking structure would be:" /name)
+    }
+}
