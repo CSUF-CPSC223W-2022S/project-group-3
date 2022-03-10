d759f6976b1435ebf806fc1653235705033f32a8
Janessa <61309969+janessa17@users.noreply.github.com>
2022-03-09 22:28:53 -0800

    Update classTime.swift

--- CSUF_Parking/classTime.swift
+++ CSUF_Parking/classTime.swift
@@ -9,5 +9,14 @@ import Foundation
 
 
 struct classTime {
-    // input code here 
+    struct classTime {
+        var hour: Int
+        var minute: Int
+        var ampm: String
+    }
+
+    func getClassTime() -> classTime {
+        let time = classTime(hour: 8, minute: 30, ampm: "AM")
+        return time
+    }
 }
