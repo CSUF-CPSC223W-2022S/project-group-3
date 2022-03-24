45a07791292f2975e801e9a5afa267f09eb4db58
Mariam Mousa <NA>
2022-03-24 08:48:20 -0700

    finally:D

--- CSUF_Parking/ViewController.swift
+++ CSUF_Parking/ViewController.swift
@@ -4,16 +4,102 @@
 //
 //  Created by csuftitan on 2/22/22.
 //
-
+import UserNotifications
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var table: UITableView!
+    
+    var models = [MyReminder]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
+    }
+    
+    @IBAction func didTapAdd() {
+    }
+    
+   // @IBAction func didTapAdd(_ sender: UIBarButtonItem) {
+   // }
+    
+    
+    @IBAction func didTapTest() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: {success, error in
+            if success {
+                self.scheduleTest()
+            }
+            else if let error = error {
+                print("error occured maybe")
+            }
+        })
+        }
+       /*
+    //@IBAction func didTapTest(_ sender: UIBarButtonItem) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: {success, error in
+            if success {
+                self.scheduleTest()
+            }
+            else if let error = error {
+                print("error occured maybe")
+            }
+        })
+   // }*/
+    
+    func scheduleTest() {
+        let content = UNMutableNotificationContent()
+        content.title = "Hello World"
+        content.sound = .default
+        content.body = "My Long Body"
+        let targetDate = Date().addingTimeInterval(10)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: targetDate), repeats: false)
+        let request = UNNotificationRequest(identifier: "some_long_id", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if error != nil {
+                print("something went wrong")
+            }
+        })
+    }
+    
+    
+    
+    
     }
 
+    
 
+
+
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row].title
+        return cell
+    }
+    
+    
+    
+}
+
+struct MyReminder {
+    let title: String
+    let date: Date
+    let identifier: String
+}
