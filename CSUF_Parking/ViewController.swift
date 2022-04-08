2a137f59aee8f717ab54351080c391a904a8da92
Janessa <61309969+janessa17@users.noreply.github.com>
2022-04-07 23:59:32 -0700

    updated date picker

--- CSUF_Parking/ViewController.swift
+++ CSUF_Parking/ViewController.swift
@@ -9,13 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var classDateTxt: UITextField!
+    
+    let datePicker = UIDatePicker()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        createDatePicker()
     }
 
-    @IBAction func buttonSegTapped(_ sender: UISegmentedControl) {
+    func createDatePicker() {
+        datePicker.preferredDatePickerStyle = .wheels
+        classDateTxt.textAlignment = .center
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+    
+        toolbar.setItems([doneBtn], animated: true)
+        classDateTxt.inputAccessoryView = toolbar
+        
+        classDateTxt.inputView = datePicker
+        
+        datePicker.datePickerMode = .date
+        
     }
     
+    @objc func donePressed() {
+        let dateformatter = DateFormatter()
+        dateformatter.dateStyle = .medium
+        dateformatter.timeStyle = .none
+        classDateTxt.text = "\(datePicker.date)"
+        self.classDateTxt.text = dateformatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
 }
 
