b4fa38ce13e3a6a98ead98107359477b0db0acdd
Janessa <61309969+janessa17@users.noreply.github.com>
2022-05-09 22:25:42 -0700

    classtime updated

--- CSUF_Parking/ViewController_classTime.swift
+++ CSUF_Parking/ViewController_classTime.swift
@@ -9,10 +9,37 @@ import UIKit
 
 class ViewController_classTime: UIViewController {
 
+    
+    @IBOutlet weak var lblDate: UILabel!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        datePicker.locale = .current
+        datePicker.date = Date()
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.addTarget(self, action: #selector(dateSelected), for: .valueChanged)
 
     }
 
-
+    @IBAction func btnSegTapped(_ sender: UISegmentedControl) {
+    switch sender.selectedSegmentIndex {
+    case 0: datePicker.preferredDatePickerStyle = .compact
+    case 1: datePicker.preferredDatePickerStyle = .inline
+    case 2: datePicker.preferredDatePickerStyle = .wheels
+    default:break
+        
+    }
+}
+    @objc
+    func dateSelected(){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .full
+        dateFormatter.timeStyle = .none
+        let date = dateFormatter.string(from: datePicker.date)
+        lblDate.text = date
+        
+    }
 }
